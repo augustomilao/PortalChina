df21d26ee569efdd0af7bdf40ccf87eca0283de2
Augusto Milão <augusto.milao01@gmail.com>
2023-05-22 15:32:28 -0300

    Criação do Component Header

---
+++ views/components/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <style>
+        .header {
+            height: 3em;
+            background-color: #D44739;
+            display: grid;
+            grid-template-columns: 20% 60% 20%;
+        }
+
+        .divGrid {
+            display: grid;
+            grid-template-columns: 50% 50%;
+            text-align: center;
+        }
+
+        .centro {
+            text-align: center;
+        }
+        .pointer{
+            cursor: pointer;
+        }
+        .pointer:hover{
+            font-weight: bold;
+        }
+        .text{
+            margin-top: 0.6em;
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <header>
+        <div class="header">
+            <div class="centro" style="font-weight: bold">
+                <p class="text">SISIMC</p>
+            </div>
+            <div class="divGrid">
+                <div class="pointer">
+                    <p class="text">New Submission</p>
+                </div>
+                <div class="pointer">
+                    <p class="text">Submission Search</p>
+                </div>
+            </div>
+            <div class="centro">
+                <p class="text">Usuario</p>
+            </div>
+        </div>
+    </header>
+
+    <div>
+
+    </div>
+
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
