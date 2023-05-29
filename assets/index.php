b030781b1f8192cbb4867899a4f38e9e646fca44
Augusto Milão <augusto.milao01@gmail.com>
2023-05-29 09:56:16 -0300

    Adicionando index de segurança

---
+++ assets/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "You are not supossed to be in here, the software owner will be notified";
+?>
--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
-$hoje = date('Y-m-d');
-$timestamp = strtotime('+10 days');
-$prazo = date('Y-m-d', $timestamp);
-
-echo $hoje . " | " . $prazo;
-
-echo "<br>";
-
-echo  $a = (strtotime($hoje) - strtotime($prazo));
-
-echo "<br>";
-// echo date('Y-m-d', $a);
-
-echo ($a/86400); # Dividir por isso par conseguir os dias
-
+header('Location: views/login.php')
 
 ?>
